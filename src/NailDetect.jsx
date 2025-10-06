00c412ce84f2fb54e24c9d892998640ae85133fd
Faraz Ahmed <masterdot707@gmail.com>
2025-10-06 11:58:23 +0500

    dev

--- src/NailDetect.jsx
+++ src/NailDetect.jsx
@@ -221,8 +221,8 @@ export default function NailDetect() {
 
       const stream = await navigator.mediaDevices.getUserMedia({
         video: {
-        //   facingMode: { exact: "environment" },
-        facingMode: 'user',
+          facingMode: { exact: "environment" },
+        // facingMode: 'user',
           width: { ideal: 1280, max: 1920 },
           height: { ideal: 720, max: 1080 },
           frameRate: { ideal: 30, max: 30 },
